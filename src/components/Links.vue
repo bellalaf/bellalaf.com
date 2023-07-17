aaeb689d96243b6be3e9cc8c39db3abfc26fba3e
Bella Lafrenais <Bella.Lafrenais@reece.com.au>
2023-07-18 01:25:59 +1000

    update links section with icons

---
+++ src/components/Links.vue
@@ -0,0 +1,55 @@
+<script setup>
+import WelcomeItem from './LinkItems.vue'
+import GithubIcon from './icons/IconGithub.vue'
+import LinkedinIcon from './icons/IconLinkedIn.vue'
+import GpnIcon from './icons/IconGpn.vue'
+import ContactIcon from './icons/IconContact.vue'
+
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <GithubIcon />
+    </template>
+    <template #heading>Github</template>
+
+    Code, commits and chaos can be found at
+    <a href="https://github.com/bellalaf" target="_blank" rel="noopener">github.com/bellalaf</a>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <LinkedinIcon />
+    </template>
+    <template #heading>LinkedIn</template>
+
+    Go on, have a stalk.
+    <a href="https://www.linkedin.com/in/bella-lafrenais/" target="_blank" rel="noopener">in/bella-lafrenais</a>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ContactIcon />
+    </template>
+    <template #heading>Contact</template>
+
+    Email me at
+    <a href="isabellalaf98@gmail.com" target="_blank" rel="noopener">isabellalaf98@gmail.com</a>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <GpnIcon />
+    </template>
+    <template #heading>GPN</template>
+
+    Interested in what we're doing over at Girls Programming Network?
+    <li>To volunteer in our Melbourne team, go to <a href="https://www.girlsprogramming.network/melbourne" target="_blank" rel="noopener">gpn/melbourne</a>.</li>
+    <li>To sign up a high school student for the next event, go to <a href="https://eng.unimelb.edu.au/engage-with-us/schools/gpn" target="_blank" rel="noopener">unimelb/gpn</a>.</li>
+    <li>Want to be involved at a larger scale? <a href="https://www.techinclusion.org.au/sponsor-us" target="_blank" rel="noopener">Sponsor us</a>.</li>
+    Just want to chat or have questions about GPN? Contact me via details above 😄
+
+  </WelcomeItem>
+
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import TheWelcome from '../components/Links.vue'
 </script>
 
 <template>
